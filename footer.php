a61245464ee0104c2ff7c27dfc9ba12dc4b86e53
marklippert <lippert@gmail.com>
2020-04-08 11:27:01 -0500

    Change Murf's email, remove outdated social media links

--- footer.php
+++ footer.php
@@ -4,21 +4,17 @@
           <?php if ($Sidebar == "") { ?>
           <aside>
             <img src="images/HirePat.png" alt="Hire Pat"><br>
-            <?php email("murf@patmccurdy.com", "Email Murf"); ?> or call 414-916-4914. Be sure to include the date, time of day, and location of the potential show to get an accurate price quote.<br>
+            <?php email("fatmurf@wi.rr.com", "Email Murf"); ?> or call 414-916-4914. Be sure to include the date, time of day, and location of the potential show to get an accurate price quote.<br>
             <br>
             <br>
 
             <a href="http://www.facebook.com/pages/pat-mccurdy/46671793550"><img src="images/icon-facebook.png" alt="Facebook" class="icon"></a>
             <a href="http://twitter.com/PatMcCurdy"><img src="images/icon-twitter.png" alt="Twitter" class="icon"></a>
-            <a href="https://plus.google.com/101880559286906913022"><img src="images/icon-googleplus.png" alt="Google+" class="icon"></a>
-            <a href="http://www.myspace.com/heypaddy"><img src="images/icon-myspace.png" alt="MySpace" class="icon"></a>
+            <a href="https://www.instagram.com/pat_mccurdy/"><img src="images/icon-instagram.png" alt="Instagram" class="icon"></a>
+            <a href="http://www.youtube.com/user/OfficialPatMcCurdy"><img src="images/icon-youtube.png" alt="YouTube" class="icon"></a>
             <a href="http://itunes.apple.com/us/artist/pat-mccurdy/id388968174"><img src="images/icon-itunes.png" alt="iTunes" class="icon"></a>
             <a href="http://www.amazon.com/gp/product/B004167BM4"><img src="images/icon-amazon.png" alt="Amazon" class="icon"></a>
             <a href="http://www.cdbaby.com/Artist/PatMcCurdy"><img src="images/icon-cdbaby.png" alt="CD Baby" class="icon"></a>
-            <a href="http://www.last.fm/music/Pat+McCurdy"><img src="images/icon-lastfm.png" alt="last.fm" class="icon"></a>
-            <a href="http://www.youtube.com/user/OfficialPatMcCurdy"><img src="images/icon-youtube.png" alt="YouTube" class="icon"></a>
-            <a href="https://www.instagram.com/explore/tags/patmccurdy/"><img src="images/icon-instagram.png" alt="Instagram" class="icon"></a>
-            <a href="http://www.flickr.com/photos/tags/patmccurdy/"><img src="images/icon-flickr.png" alt="Flickr" class="icon"></a>
             <a href="rss.xml"><img src="images/icon-rss.png" alt="RSS" class="icon"></a>
           </aside>
           <?php } ?>
