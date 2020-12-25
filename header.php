7822078e9bd63bf3d530a819f0aa7acda2f3c499
marklippert <lippert@gmail.com>
2020-12-25 15:05:06 -0600

    Add tracking code

--- header.php
+++ header.php
@@ -33,6 +33,15 @@ function email($address, $name="") {
         $("a[href$='.pdf']").prop('target', 'new');
       });
     </script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-9892672-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-9892672-2');
+    </script>
   </head>
   <body>
 
