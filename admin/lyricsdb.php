b21a6792d7b86f3bc4d4635298994c37f1429931
Mark Lippert <lippert@gmail.com>
2024-06-20 12:18:34 -0500

    Update SQL to prepared statements

--- admin/lyricsdb.php
+++ admin/lyricsdb.php
@@ -1,20 +1,29 @@
 <?php
 include("../inc/dbconfig.php");
 
+if ($_GET['a'] == "add" || $_GET['a'] == "edit") $array = [gremlins($_POST['title']), gremlins($_POST['lyrics']), $_POST['album'], gremlins($_POST['album_track']), $_POST['band'], gremlins($_POST['apple']), gremlins($_POST['amazon']), "yes"];
+
 switch ($_GET['a']) {
   case "add":
-    $query = "INSERT INTO lyrics (title,lyrics,album,album_track,band,apple,amazon,approved) VALUES ('" . $mysqli->real_escape_string($_POST['title']) . "','" . $mysqli->real_escape_string($_POST['lyrics']) . "','" . $_POST['album'] . "','" . $_POST['album_track'] . "','" . $_POST['band'] . "','" . $_POST['apple'] . "','" . $_POST['amazon'] . "', 'yes')";
+    $sql = "INSERT INTO lyrics (title, lyrics, album, album_track, band, apple, amazon, approved) VALUES (?,?,?,?,?,?,?,?)";
+
     break;
   case "edit":
-    $query = "UPDATE lyrics SET title = '" . $mysqli->real_escape_string($_POST['title']) . "', lyrics = '" . $mysqli->real_escape_string($_POST['lyrics']) . "', album = '" . $_POST['album'] . "', album_track = '" . $_POST['album_track'] . "', band = '" . $_POST['band'] . "', apple = '" . $_POST['apple'] . "', amazon = '" . $_POST['amazon'] . "', approved = 'yes' WHERE id = '" . $_POST['id'] . "'";
+    $sql = "UPDATE lyrics SET title = ?, lyrics = ?, album = ?, album_track = ?, band = ?, apple = ?, amazon = ?, approved = ? WHERE id = ?";
+    $array[] = $_POST['id'];
+
     break;
   case "delete":
     $table = ($_GET['l'] == "holding") ? "lyrics_holding" : "lyrics";
-    $query = "DELETE FROM $table WHERE id = '" . $_GET['id'] . "'";
+    $sql = "DELETE FROM $table WHERE id = ?";
+    $array = [$_GET['id']];
+    
     break;
 }
 
-$mysqli->query($query) or die($mysqli->error);
+$mysqli->execute_query($sql, $array);
+
+$mysqli->close();
 
 $go = (isset($_REQUEST['l'])) ? "lyricsindex.php?filter=" . $_REQUEST['l'] : "lyricsindex.php";
 
